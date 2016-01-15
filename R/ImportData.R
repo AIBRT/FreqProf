280f8b52713bbd3c554cfbc26a6e24a05710d7a6
dethfromabove <mejiajon@gmail.com>
2016-01-14 21:27:29 -0800

    Reformatted ImportData.R

--- R/ImportData.R
+++ R/ImportData.R
@@ -12,26 +12,26 @@
 #' @examples
 #'  \donttest{
 #'  # Select a file
-#'  importData(filename = system.file("extdata/S58-1-1.bin", 
+#'  import_Data(filename = system.file("extdata/S58-1-1.bin", 
 #'             package = "FreqProf"))
 #'  }
-importData = function(filename = file.choose()) {
+import_Data = function(filename = file.choose()) {
   # this function reads a file, whose extension is either csv, bin or fpw,
   # and imports it as a data.frame
   
-  file.extension = tolower (substr (filename, 
-                                  nchar (filename) - 2, nchar (filename)))
+  file.extension = tolower(substr(filename, 
+                                  nchar(filename) - 2, nchar(filename)))
   
-  data = switch (file.extension,
-                 csv = read.csv (filename),
-                 bin = read.bin (filename),
-                 fpw = read.fpw (filename))
+  data = switch(file.extension,
+                csv = read.csv(filename),
+                bin = read.bin(filename),
+                fpw = read.fpw(filename))
   
-  if (is.null (data)){
-    stop ("file extension must be either csv, fpw, or bin")  
+  if(is.null(data)){
+    stop("file extension must be either csv, fpw, or bin")  
   }
   
-  return (data)
+  return(data)
 }
 
 #' Reads the data in the file "filename", which is supposed to be a .bin file
@@ -51,36 +51,36 @@ read.bin = function(filename){
 >>>>>>> upstream/master
   
   # scanning the file, line by line
-  file.scan = scan (file = filename,
-                    what = "character",
-                    sep = "\n",
-                    quiet = T)
+  file.scan = scan(file = filename,
+                   what = "character",
+                   sep = "\n",
+                   quiet = T)
   
   # looking for the line that starts with an asterisk
-  N = which (sapply (X = 1:length (file.scan), 
+  N = which(sapply(X = 1:length(file.scan), 
                    FUN = function(x) {
-                     substr (file.scan[x], 1, 1) == "*"
+                   substr(file.scan[x], 1, 1) == "*"
                      }
                      ))
-  file.data = file.scan [(N + 2):length (file.scan)]
+  file.data = file.scan[(N + 2):length(file.scan)]
   
   # removing "<" at the end of line
-  for (j in 1:length (file.data)) {
-    file.data[j] = substr (file.data[j], 1, nchar (file.data[j]) - 1)
+  for(j in 1:length(file.data)) {
+    file.data[j] = substr(file.data[j], 1, nchar(file.data[j]) - 1)
   }
   
   # converting these strings into a matrix
-  result = matrix (data = NA, nrow = nchar (file.data[1]), 
-                   ncol = length (file.data))
-  for (j in 1:dim (result)[2]) {
-    result[, j] = sapply (X = 1:nchar (file.data[j]), 
+  result = matrix(data = NA, nrow = nchar(file.data[1]), 
+                   ncol = length(file.data))
+  for(j in 1:dim(result)[2]) {
+    result[, j] = sapply(X = 1:nchar(file.data[j]), 
                           FUN = function(x) {
-                            as.numeric (substr (file.data[j], x, x))
+                            as.numeric(substr(file.data[j], x, x))
                             }
                           )
   }
   
-  return (as.data.frame (result))
+  return(as.data.frame(result))
 }
 
 #' Reads the data in the file "filename", which is supposed to be a .fpw file
@@ -96,14 +96,14 @@ read.fpw = function(filename) {
 read.fpw = function(filename){
 >>>>>>> upstream/master
   
-  file.scan = scan (file = filename,
+  file.scan = scan(file = filename,
                    what = "character",
                    sep = "\n",
                    quiet = T)
   # looking for the line that indicates "[DATA]"
-  N = which (sapply (X = 1:length (file.scan), 
-                     FUN = function(x) {
-                       file.scan[x] == "[DATA]"
+  N = which(sapply(X = 1:length(file.scan), 
+                   FUN = function(x) {
+                   file.scan[x] == "[DATA]"
                        }
                      ))
   
@@ -112,8 +112,8 @@ read.fpw = function(filename){
                       sep = " ", strip.white = F)
   
   # as the lines begin with a " ", the first column is read as NA
-  result = result[, 2:ncol (result)]
-  names(result) = paste("V", 1:ncol (result), sep = "")
+  result = result[, 2:ncol(result)]
+  names(result) = paste("V", 1:ncol(result), sep = "")
   
-  return (result)
+  return(result)
 }
